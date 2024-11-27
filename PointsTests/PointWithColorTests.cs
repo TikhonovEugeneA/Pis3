19b75ed18c1feb4f0665725a2154ee9eee7de74a
tikh <Evgeniy@NB_EVGENIY>
2024-11-27 12:06:34 +0700

    Второй коммит

--- PointsTests/PointWithColorTests.cs
+++ PointsTests/PointWithColorTests.cs
@@ -30,5 +30,58 @@ namespace PointsTests
             Assert.AreEqual(point.Item1, null);
         }
     }
-    
+    [TestClass]
+    public class PointWithWeightTests
+    {
+        [TestMethod]
+        public void PointWithWeight_ShouldSetProperties()
+        {
+            //arrange
+            double weight = -10;
+            var point = (111, 111);
+            //act
+            Program.PointWithWeight pointWithColor = new Program.PointWithWeight(point, weight);
+
+            //assert
+            ReferenceEquals(pointWithColor, "111;111 -10");
+        }
+
+        [TestMethod]
+        public void PointWithWeight_AreEqual()
+        {
+            //arrange 
+            (double?, double) point = (null, 111);
+
+            //act and assert
+            Assert.AreEqual(point.Item1, null);
+        }
+    }
+    [TestClass]
+    public class PointWithSpeedTests
+    {
+        [TestMethod]
+        public void PointWithSpeed_ShouldSetProperties()
+        {
+            //arrange
+            double weight = -10;
+            var point = (111, 111);
+
+            //act
+            Program.PointWithSpeed pointWithColor = new Program.PointWithSpeed(point, weight);
+
+            //assert
+            ReferenceEquals(pointWithColor, "111;111 -10");
+        }
+
+        [TestMethod]
+        public void PointWithWeight_AreEqual()
+        {
+            //arrange 
+            (double?, double) point = (null, 111);
+
+            //act and assert
+            Assert.AreEqual(point.Item1, null);
+        }
+    }
+
 }
