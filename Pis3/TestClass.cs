4767ab253d456d57824f80574ea7f76864a0980e
tikh <Evgeniy@NB_EVGENIY>
2024-11-30 10:29:18 +0700

    Добавлена заглушка

---
+++ Pis3/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pis3
+{
+    public class TestClass
+    {
+        public static int CountZeroes(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input),"Строка не может быть пустой");
+            return input.Count(c=>c=='0');
+        }
+    }
+}
